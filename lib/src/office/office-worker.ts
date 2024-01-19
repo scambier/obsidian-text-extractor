b89ce29fc984d7e657c1b682673d0eb36ea0cec3
demig00d <28487425+demig00d@users.noreply.github.com>
2024-01-19 16:03:59 +0300

    Add xlsx support

--- lib/src/office/office-worker.ts
+++ lib/src/office/office-worker.ts
@@ -1,8 +1,14 @@
 import mammoth from "mammoth";
+import { utils as sheetUtils, read as xlsxRead } from 'xlsx'
 
 onmessage = async evt => {
   try {
-    let text = await extractWordText(evt.data.data)
+    let text = ''
+    if (evt.data.extension == 'docx') {
+      text = await extractWordText(evt.data.data)
+    } else if (evt.data.extension == 'xlsx') {
+      text = await extractExcelText(evt.data.data)
+    }
     self.postMessage({ text })
   } catch (e) {
     console.info('Text Extractor - Could not extract text from ' + evt.data.name)
@@ -10,7 +16,19 @@ onmessage = async evt => {
   }
 }
 
+
 async function extractWordText(arr: Uint8Array): Promise<string> {
   const result = await mammoth.extractRawText({ arrayBuffer: arr })
   return result.value
+}
+
+async function extractExcelText(arr: Uint8Array): Promise<string> {
+  const workbook = xlsxRead(arr, { type: 'buffer' })
+  let text = ''
+  const sheets = workbook.SheetNames
+  for (const sheet of sheets) {
+    text += `${sheet} `
+    text += sheetUtils.sheet_to_txt(workbook.Sheets[sheet])
+  }
+  return text
 }
